4abf4ccb9f0f7bb0464da88b2adfcf1f9338e8f1
amal <howareyouami@gmail.com>
2023-09-30 17:40:43 +0200

    checkAgeFilter tests

--- utils/filter/checkAgeFilter.test.js
+++ utils/filter/checkAgeFilter.test.js
@@ -1,12 +1,27 @@
 import { checkAgeFilter } from ".";
 
 const testConfig = [
+  // filter, value, result
+  // Test for 'any'
   ["any", 0, true],
+  // Tests for '18to20'
   ["18to20", 1, false],
   ["18to20", 17, false],
   ["18to20", 18, true],
+  ["18to20", 19, true],
   ["18to20", 20, true],
   ["18to20", 21, false],
+  // Tests for '31to45'
+  ["31to45", 30, false],
+  ["31to45", 31, true],
+  ["31to45", 32, true],
+  ["31to45", 45, true],
+  ["31to45", 46, false],
+  // Tests for 'above45'
+  ["above45", 40, false],
+  ["above45", 45, false],
+  ["above45", 46, true],
+  ["above45", 100, true],
 ];
 
 describe("checkAgeFilter", () => {
