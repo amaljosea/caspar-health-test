33a55b1a8afb8ec90856af774645ae51bdae08cf
amal <howareyouami@gmail.com>
2023-09-30 19:03:07 +0200

    Debounce

--- hooks/useFilteredPatients.ts
+++ hooks/useFilteredPatients.ts
@@ -1,12 +1,15 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext, useMemo } from "react";
 import { PatientContext } from "@/context/PatientContext";
 import { getFilteredPatients } from "@/utils/filter/getFilteredPatients";
 import { getSortedPatients } from "@/utils/getSortedPatients";
+import { useDebounce } from "./useDebounce";
 
 export const useFilteredPatients = () => {
   const { patients, filterControls } = useContext(PatientContext);
 
-  const { gender, age, search, sort } = filterControls;
+  const { gender, age, search: liveSearch, sort } = filterControls;
+
+  const search = useDebounce(liveSearch);
 
   const { filteredAndSortedPatients } = useMemo(() => {
     const { filteredPatients } = getFilteredPatients({
