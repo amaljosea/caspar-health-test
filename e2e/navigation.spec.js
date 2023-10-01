ceaa5b6d88d9c00ad83d4ad53b16b373f09f84e6
amal <howareyouami@gmail.com>
2023-10-01 21:09:03 +0200

    Basic test

--- e2e/navigation.spec.js
+++ e2e/navigation.spec.js
@@ -1,12 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the about page", async ({ page }) => {
+test("should navigate to the detail page on patient click", async ({
+  page,
+}) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
   // Find an element with the text '46' and click on it
   await page.click("text=46");
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("http://localhost:3000/detail/46");
+  await expect(page).toHaveURL("/detail/46");
   // The new page should contain an h1 with "Detail"
   await expect(page.locator("h1")).toContainText("Detail");
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
+  use: {
+    baseURL: "http://localhost:3000/",
+  },
 };
 
 export default config;
